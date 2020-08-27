be9ec3bca1e620075211b917b06838dcffb887c2
paul-shannon <paul.thurmond.shannon@gmail.com>
2020-08-27 10:04:54 -0700

    developed for R 4.0.2 on khaleesi

---
+++ R/installCommonPackages.R
@@ -0,0 +1,71 @@
+biocGet <- function (pkgs){
+   library(BiocManager)
+   BiocManager::install(pkgs)
+   }
+
+BiocManager::install(ask=FALSE, update=TRUE)
+
+
+bioc.basics <- c("bit64",
+                 "blob",
+                 "stringi",
+                 "BiocGenerics",
+                 "org.Hs.eg.db",
+                 "phastCons7way.UCSC.hg38",
+                 "phastCons100way.UCSC.hg38",
+                 "MotifDb",
+                 "VariantAnnotation",
+                 "AnnotationDbi",
+                 "RPostgreSQL",
+                 "biomaRt",
+                 "Biostrings",
+                 "S4Vectors",
+                 "XVector",
+                 "glmnet",
+                 "foreach",
+                 "Matrix",
+                 "GenomeInfoDb",
+                 "IRanges",
+                 "GenomicRanges",
+                 "GenomicFeatures",
+                 "GenomicScores",
+                 "annotatr",
+                 "ggplot2")
+
+other.pkgs <- c("prettyunits",
+                "devtools",
+                "motifmatchr",
+                "TFBSTools",
+                "universalmotif",
+                "ggseqlogo",
+                "yaml",
+                "later",
+                "DT",
+                "V8",
+                "shiny",
+                "shinydashboard",
+                "shinyjs",
+                "colourpicker",
+                "RColorBrewer")
+pkgs <- c(bioc.basics, other.pkgs)
+
+for(pkg in pkgs){
+   suppressWarnings(
+      needed <- !require(pkg, character.only=TRUE, quiet=TRUE)
+      )
+   printf("%s needed? %s", pkg, needed)
+   if(needed)
+      BiocManager::install(pkg, ask=FALSE, update=TRUE)
+   } # for
+
+library(devtools)
+github.packages <- c("paul-shannon/cyjShiny",
+                     "paul-shannon/shinyModules",
+                     "priceLab/trena",
+                     "priceLab/TrenaProject",
+                     "priceLab/TrenaProjectHG38",
+                     "priceLab/TrenaProjectErythropoiesis")
+
+for(pkg in github.packages){
+   install_github(pkg)
+   }
